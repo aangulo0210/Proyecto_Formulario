c0d4df93be42943b9708de4c9b81d1696867ae56
aangulo0210 <andres.alexander0210@gmail.com>
2024-11-02 18:00:38 -0500

    Final de la clase

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,6 @@
+//Contrasea superior o igual a 8 digitos
+//Que los espacios de texto no esten vacios
+
 const boton = document.getElementById("enviar");
 boton.addEventListener ("click", () => {
     alert(`No se ha digitado valor alguno`);
